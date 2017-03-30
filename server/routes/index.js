21c7487bd88df2bd0c605bdebc6be875cf9cbd70
haikuoliu <haikuo.liu.cu@gmail.com>
2017-03-29 20:52:33 -0400

    add first 3 api

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,29 +8,52 @@ router.get('/', function (req, res) {
     res.render('index', { user : req.user });
 });
 
-
-router.get('/register', function(req, res) {
-    res.render('register', { });
-});
-
 router.post('/register', function(req, res) {
     Account.register(new Account({ username : req.body.username, birth : req.body.birth, gender : req.body.gender, email : req.body.email, phone : req.body.phone}), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            res.write(JSON.stringify({status: "fail", result: {msg: "Register fail"}}));
+            res.end();
+            // return res.render('register', { account : account });
         }
-
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/profile');
-        });
+        else {
+            res.write(JSON.stringify({status: "succ", result: {username: req.body.username}}));
+            res.end();
+        }
     });
 });
 
-router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
+router.post('/login', function(req, res) {
+    Account.authenticate()(req.body.username, req.body.password, function (err, user, options) {
+        if (err) {
+            res.write(JSON.stringify({status: "fail", result: {msg: "Fail due to unknow err"}}));
+            res.end();  
+        }
+        if (user === false) {
+            res.write(JSON.stringify({status: "fail", result: {msg: "Fail due to incorrect username or password"}}));
+            res.end();  
+        } else {
+            res.write(JSON.stringify({status: "succ", result: {username: req.body.username}}));
+            res.end();
+        }
+    });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/profile');
+router.get('/getByUName', function(req, res) {
+    Account.findOne({ 'username' : req.query.username}, 'username birth gender email phone', function (err, account) {
+        if (err) {
+            res.write(JSON.stringify({status: "fail", result: {msg: "Can't find user profile"}}));
+            res.end();
+        }
+        // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
+        // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
+        else {
+            res.write(JSON.stringify({status: "succ", result: {username: account.username, 
+                birth : account.birth, gender : account.gender, 
+                email : account.email, phone : account.phone
+            }}));
+            res.end();
+        }
+    });
 });
 
 router.get('/logout', function(req, res) {
@@ -38,15 +61,15 @@ router.get('/logout', function(req, res) {
     res.redirect('/');
 });
 
-router.get('/profile', function (req, res) {
-    Account.findOne({ 'username' : req.session.passport.user}, 'username birth gender email phone', function (err, account) {
-        if (err) return handleError(err);
-        // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
-        console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
-        res.status(200).send("cool");
-    });
-    // res.status(200).send("cool");
-})
+// router.get('/profile', function (req, res) {
+//     Account.findOne({ 'username' : req.session.passport.user}, 'username birth gender email phone', function (err, account) {
+//         if (err) return handleError(err);
+//         // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
+//         console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
+//         res.status(200).send("cool");
+//     });
+//     // res.status(200).send("cool");
+// })
 
 router.get('/ping', function(req, res){
     res.status(200).send("pong!");
