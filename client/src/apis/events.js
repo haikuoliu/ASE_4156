3acd2216a4eb6896d7c81745b238e6dece5d6656
tsszh <songzhpku@gmail.com>
2017-04-06 10:41:12 -0400

    Event

--- client/src/apis/events.js
+++ client/src/apis/events.js
@@ -3,10 +3,10 @@ import hostname from 'SRC/utils/host'
 const events = {
   switchLikeStatus: (uid, eid, isLike) => `${hostname()}/api/likes?uid=${uid}&eid=${eid}&isLike=${isLike}`,
   getPostsOfUser: (uid, myid) => `${hostname()}/api/posts/user?uid=${uid}&myid=${myid}`,
-  getSingleEvent: (eid, myid) => `${hostname()}/api/event?eid=${eid}&myid=${myid}`,
+  getSingleEvent: (eid, myid) => `${hostname()}/getPost?eid=${eid}&username=${myid}`,
   getComments: (eid) => `${hostname()}/api/event/comments?eid=${eid}`,
   createComments: `${hostname()}/api/event/comments`,
-  editEvent: `${hostname()}/api/event/create`,
+  editEvent: `${hostname()}/addPost`,
   deleteEvent: `${hostname()}/api/event/delete`
 }
 
