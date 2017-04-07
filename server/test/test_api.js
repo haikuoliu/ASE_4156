c12643466cf305bb990ca0e88a988385c4754f9f
haikuoliu <haikuo.liu.cu@gmail.com>
2017-04-06 22:51:52 -0400

    finish test

--- server/test/test_api.js
+++ server/test/test_api.js
@@ -12,7 +12,6 @@ describe('test user login functions', function() {
             password: "zehao"
         };
         request.post({url:'http://localhost:3000/login', formData: formData}, function optionalCallback(err, res, body) {
-            console.log(body);
             var body = JSON.parse(body);
             expect(err).to.equal(null);
             expect(body.status).to.equal("succ");
@@ -33,7 +32,6 @@ describe('test user login functions', function() {
             phone: "123"
         };
         request.post({url:'http://localhost:3000/register', formData: formData}, function optionalCallback(err, res, body) {
-            console.log(body);
             var body = JSON.parse(body);
             expect(err).to.equal(null);
             expect(body.status).to.equal("succ");
@@ -41,9 +39,50 @@ describe('test user login functions', function() {
         });
     });
 
+});
+
+
+
+describe('test get info functions', function() {           
+    this.timeout(15000);
     it('test getBasicInfo', function(done) {
         var request = require('request');
         request.get({url:'http://localhost:3000/getBasicInfo?username=zehao'}, function optionalCallback(err, res, body) {
+            var body = JSON.parse(body);
+            expect(err).to.equal(null);
+            expect(body.status).to.equal("succ");
+            expect(body.result.username).to.equal("zehao");
+            expect(body.result.birth).to.equal("2017-03-08");
+            expect(body.result.gender).to.equal("male");
+            expect(body.result.email).to.equal("aa@gmail.com");
+            expect(body.result.phone).to.equal("adfasd");
+            done();
+        });
+    });
+
+    it('test getPetsInfo', function(done) {
+        var request = require('request');
+        request.get({url:'http://localhost:3000/getPetsInfo?username=test2'}, function optionalCallback(err, res, body) {
+            var body = JSON.parse(body);
+            expect(err).to.equal(null);
+            expect(body.status).to.equal("succ");
+            done();
+        });
+    });
+
+    it('test getCentersInfo', function(done) {
+        var request = require('request');
+        request.get({url:'http://localhost:3000/getCentersInfo?username=test2'}, function optionalCallback(err, res, body) {
+            var body = JSON.parse(body);
+            expect(err).to.equal(null);
+            expect(body.status).to.equal("succ");
+            done();
+        });
+    });
+
+    it('test getOrder', function(done) {
+        var request = require('request');
+        request.get({url:'http://localhost:3000/getOrder?username=test2'}, function optionalCallback(err, res, body) {
             var body = JSON.parse(body);
             expect(err).to.equal(null);
             expect(body.status).to.equal("succ");
@@ -51,5 +90,40 @@ describe('test user login functions', function() {
         });
     });
 });
+
+
+// describe('test post functions', function() {           
+//     this.timeout(15000);
+
+//     // it('test addPost', function(done) {
+//     //     var request = require('request');
+//     //     var formData = {     
+//     //         username: "zehao",
+//     //         birth: '2017-03-08',
+//     //         gender: 'male',
+//     //         email: 'aa@gmail.com',
+//     //         phone: 'adfasd'
+//     //     };
+//     //     request.post({url:'http://localhost:3000/addPost', formData: formData}, function optionalCallback(err, res, body) {
+//     //         var body = JSON.parse(body);
+//     //         expect(err).to.equal(null);
+//     //         expect(body.status).to.equal("succ");
+//     //         done();
+//     //     });
+//     // });
+
+//     it('test getPost', function(done) {
+//         var request = require('request');
+//         request.get({url:'http://localhost:3000/getPost?username=zehao'}, function optionalCallback(err, res, body) {
+//             console.log(body);
+//             var body = JSON.parse(body);
+            
+//             expect(err).to.equal(null);
+//             expect(body.status).to.equal("succ");
+//             done();
+//         });
+//     });
+
+// });
 
 
