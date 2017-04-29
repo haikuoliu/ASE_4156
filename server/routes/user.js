a4c710b4b0c3097ff400bb8c042c8bdc93b45e55
Kai He <kh2789@columbia.edu>
2017-04-28 20:23:47 -0400

    add update profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -57,6 +57,40 @@ router.get('/basicInfo', function(req, res) {
     });
 });
 
+router.put('/basicInfo', function(req, res) {
+    Account.findOne({ 'username' : req.query.username}, 'username birth gender email phone', function (err, account) {
+        if (err) {
+            res.write(JSON.stringify({status: "fail", result: {msg: "Can't find user profile"}}));
+            res.end();
+        }
+        // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
+        // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
+        else {
+            // console.log("account: " +account);
+
+            if (account == null) {
+                res.write(JSON.stringify({status: "fail", result: {msg: "Can't find user profile"}}));
+                res.end();
+            }
+            account.birth = req.body.birth;
+            account.gender = req.body.gender;
+            account.email = req.body.email;
+            account.phone = req.body.phone;
+            account.save(function (err) {
+                if (err) {
+                    res.write(JSON.stringify({status: "fail", result: {msg: "Can't save"}}));
+                    res.end();
+                } else {
+                    res.write(JSON.stringify({status: "succ", result: {username: req.body.username}}));
+                    res.end();
+                }
+            })
+        }
+    });
+});
+
+
+
 // route for logging out
 // router.get('/logout', function(req, res) {
 //     req.logout();
