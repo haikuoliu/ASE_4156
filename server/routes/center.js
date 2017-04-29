ba8858b279a300540a407bb7340a3e1dbb616303
Kai He <kh2789@columbia.edu>
2017-04-29 11:13:59 -0400

    change db schema a little, convert all street to latlng and zip

--- server/routes/center.js
+++ server/routes/center.js
@@ -33,21 +33,53 @@ function searchAddress(address, callback) {
 }
 
 
-// router.get('/getCentersInfo', function(req, res) {
-// 	console.log("centers!!");
-//     Account.findOne({ 'username' : req.query.username}, 'username centersInfo', function (err, account) {
-//         if (err) {
-//             res.write(JSON.stringify({status: "fail", result: {msg: "Can't find centers information"}}));
-//             res.end();
-//         }
-//         // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
-//         // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
-//         else {
-//             res.write(JSON.stringify({status: "succ", result: {centersInfo: account.centersInfo}}));
-//             res.end();
-//         }
-//     });
-// });
+
+router.get('/updateCenter', function(req, res) {
+    Account.find('username centersInfo', function (err, account) {
+        if (err) {
+            res.write(JSON.stringify({status: "fail", result: {msg: "Can't find centers information"}}));
+            res.end();
+        }
+        // res.render('/profile', {username : account.username, birth : account.birth, gender : account.gender, email : account.email, phone : account.phone});
+        // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
+        else {
+            account.forEach(function(acc, i)  {
+                acc.centersInfo.forEach(function ( center, j) {
+                    console.log("street = " + account[i].centersInfo[j].location.street);
+                    searchAddress(account[i].centersInfo[j].location.street, function(coord,zipcode) {
+                        console.log("coord = " + coord + " zip = " + zipcode);
+                        if (coord!=null) {
+                            account[i].centersInfo[j].location.lat = coord.lat;
+                            account[i].centersInfo[j].location.lng = coord.lng;
+                            account[i].centersInfo[j].location.zip = zipcode;
+
+                            console.log("lat = " + coord.lat + " lng = " + coord.lng + " zip = " + zipcode);
+
+                            account[i].save(function (err) {
+                                if (err) {
+                                    // res.write(JSON.stringify({status: "fail", result: {msg: "Can't save"}}));
+                                    // res.end();
+                                    console.log("write error");
+                                }
+                                else {
+                                    console.log("write once");
+                                }
+                            })
+
+                        } else {
+                            // res.write(JSON.stringify({status: "fail", result: {msg: "Search request failed"}}));
+                            // res.end();
+                            console.log("corrd == null");
+                        }
+                    });
+                })
+            });
+        }
+    });
+});
+
+
+
 
 router.get('/centersInfo', function(req, res) {
     Account.findOne({ 'username' : req.query.username}, 'username centersInfo', function (err, account) {
@@ -59,7 +91,7 @@ router.get('/centersInfo', function(req, res) {
         // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
         else {
             for (var i = 0; i < account.centersInfo.length; i++) {
-                if (req.query.cid === account.centersInfo[i].cid.toString()) {
+                if (req.query.cid === account.centersInfo[i].cid) {
                     res.write(JSON.stringify({status: "succ", result: {centersInfo: account.centersInfo[i]}}));
                     res.end();
                     return;
@@ -80,10 +112,11 @@ router.post('/centersInfo', function (req, res) {
             res.end();
         } else {
             var objectId = new ObjectID();
+            var id = objectId.toString();
             searchAddress(req.body.street, function(coord,zipcode) {
                 if (coord!=null) {
                     account.centersInfo.push({
-                        cid: objectId,
+                        cid: id,
                         title: req.body.title,
                         content: req.body.content,
                         location: {
@@ -102,7 +135,7 @@ router.post('/centersInfo', function (req, res) {
                             res.write(JSON.stringify({status: "fail", result: {msg: "Can't save"}}));
                             res.end();
                         } else {
-                            res.write(JSON.stringify({status: "succ", result: {username: req.body.username, cid: objectId}}));
+                            res.write(JSON.stringify({status: "succ", result: {username: req.body.username, cid: id}}));
                             res.end();
                         }
                     })
@@ -126,7 +159,7 @@ router.put('/centersInfo', function (req, res) {
         } else {
             var i;
             for (i = 0; i < account.centersInfo.length; i++) {
-                if (req.body.cid === account.centersInfo[i].cid.toString()) {
+                if (req.body.cid === account.centersInfo[i].cid) {
                     break;
                 }
             }
@@ -189,7 +222,7 @@ router.delete('/centersInfo', function(req, res) {
         // console.log("username : " + account.username + " birth : " + account.birth + " gender : " + account.gender + " email : " + account.email + " phone " + account.email);
         else {
             for (var i = 0; i < account.centersInfo.length; i++) {
-                if (req.query.cid === account.centersInfo[i].cid.toString()) {
+                if (req.query.cid === account.centersInfo[i].cid) {
                     //delete account.centersInfo[i];
                     account.centersInfo.splice(i, 1);
                     account.save(function (err) {
