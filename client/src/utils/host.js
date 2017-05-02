d6fa2ba65b338596874803f90bfcf3bfd4ac6fc2
tsszh <songzhpku@gmail.com>
2017-05-02 14:42:01 -0400

    Fix FE Test Host Name

--- client/src/utils/host.js
+++ client/src/utils/host.js
@@ -3,7 +3,7 @@ let hostname // eslint-disable-line
 if (process.env.NODE_ENV === 'production') {
   hostname = () => 'http://localhost:3000'
 } else if (process.env.NODE_ENV === 'test') {
-  hostname = () => 'http://localhost:3000'
+  hostname = () => 'http://localhost:8888'
 } else {
   hostname = () => 'http://localhost:3000'
 }
